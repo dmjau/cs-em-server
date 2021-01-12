5a61eacf41a9edd185f14a0d597ab0acb4e10809
dmjau <dmjaume.bsas@gmail.com>
2021-01-12 17:29:31 -0300

    added employee model and migration

---
+++ migrations/20210112202311-create-employee.js
@@ -0,0 +1,47 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('employees', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      company_id: {
+        type: Sequelize.INTEGER
+      },
+      first_name: {
+        type: Sequelize.STRING
+      },
+      last_price: {
+        type: Sequelize.STRING
+      },
+      birth_day: {
+        type: Sequelize.DATE
+      },
+      employee_type: {
+        type: Sequelize.STRING
+      },
+      designer_type: {
+        type: Sequelize.STRING
+      },
+      programming_language: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date()
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date()
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('employees');
+  }
+};
---
+++ models/employee.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const employee = sequelize.define(
+    'employee',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      company_id: DataTypes.INTEGER,
+      first_name: DataTypes.STRING,
+      last_price: DataTypes.STRING,
+      birth_day: DataTypes.DATE,
+      employee_type: DataTypes.STRING,
+      designer_type: DataTypes.STRING,
+      programming_language: DataTypes.STRING
+    },
+    {
+      tableName: 'employees'
+    }
+  );
+  item.associate = function (models) {
+    item.belongsTo(models.company, {
+      as: 'company',
+      foreignKey: 'company_id'
+    });
+  };
+  return item;
+};
