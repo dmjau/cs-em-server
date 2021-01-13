aaf70c28621e7827fee1f8a0e682d472fb0807c2
dmjau <dmjaume.bsas@gmail.com>
2021-01-13 11:18:31 -0300

    created employees routes

---
+++ controllers/employeeCtrl.js
@@ -0,0 +1,132 @@
+const { company, employee, Sequelize } = require('./../models');
+const Op = Sequelize.Op;
+let self = {};
+
+//GET all employees
+self.getAll = async (req, res) => {
+  try {
+    let data = await employee.findAll({
+      attributes: [
+        'id',
+        'company_id',
+        'first_name',
+        'last_name',
+        'birth_day',
+        'employee_type',
+        'designer_type',
+        'programming_language'
+      ],
+      include: [
+        {
+          model: company,
+          as: 'company',
+          attributes: ['id', 'name']
+        }
+      ]
+    });
+    return res.json({
+      data: data
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: 'error',
+      data: error
+    });
+  }
+};
+
+//GET employee by id
+self.getById = async (req, res) => {
+  try {
+    let id = req.params.id;
+    let data = await employee.findOne({
+      attributes: [
+        'id',
+        'company_id',
+        'first_name',
+        'last_name',
+        'birth_day',
+        'employee_type',
+        'designer_type',
+        'programming_language'
+      ],
+      include: [
+        {
+          model: company,
+          as: 'company',
+          attributes: ['id', 'name']
+        }
+      ],
+      where: {
+        id: id
+      }
+    });
+    return res.json({
+      data: data
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: 'error',
+      data: error
+    });
+  }
+};
+
+//POST new employee
+self.register = async (req, res) => {
+  try {
+    let body = req.body;
+    let data = await employee.create(body);
+    return res.json({
+      data: data
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: 'error',
+      data: error
+    });
+  }
+};
+
+//UPDATE employee by id
+self.update = async (req, res) => {
+  try {
+    let id = req.params.id;
+    let body = req.body;
+    let data = await employee.update(body, {
+      where: {
+        id: id
+      }
+    });
+    return res.json({
+      data: data
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: 'error',
+      data: error
+    });
+  }
+};
+
+//DELETE employee by id
+self.delete = async (req, res) => {
+  try {
+    let id = req.params.id;
+    let data = await employee.destroy({
+      where: {
+        id: id
+      }
+    });
+    return res.json({
+      data: data
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: 'error',
+      data: error
+    });
+  }
+};
+
+module.exports = self;
--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,26 @@
 const companyCtrl = require('./../controllers/companyCtrl');
+const employeeCtrl = require('./../controllers/employeeCtrl');
 
 module.exports = function (express) {
   const route = express.Router();
 
   //Company routes
+  /* GET all companies */
   route.get('/companies', companyCtrl.get);
+  /* UPDATE company by id */
   route.put('/companies/:id', companyCtrl.update);
+
+  //Employees routes
+  /* GET all employees */
+  route.get('/employees', employeeCtrl.getAll);
+  /* GET one employee by id */
+  route.get('/employees/:id', employeeCtrl.getById);
+  /* POST a new employee */
+  route.post('/employees', employeeCtrl.register);
+  /* UPDATE employee by id */
+  route.put('/employees/:id', employeeCtrl.update);
+  /* DELETE employee by id */
+  route.delete('/employees/:id', employeeCtrl.delete);
 
   return route;
 };
